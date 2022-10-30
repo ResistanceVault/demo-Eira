8267368ff019edbe0b28319ff39801d16ecd24f5
David Vandensteen <dvandensteen@gmail.com>
2022-10-30 10:25:43 +0100

    Update : refactoring install script

--- scripts/neocore-setup.ps1
+++ scripts/neocore-setup.ps1
@@ -1,41 +1,37 @@
 Param (
-  [String] $neobuildData
+  [Parameter(Mandatory=$true)][String] $Path
 )
-[String] $neobuildTemp = "$env:TEMP\neocore"
+
 [String] $toolsHost = "http://azertyvortex.free.fr/download"
 
 function cdTemplate {
   Write-Host "setup cd_template"
-  Download $toolsHost/neobuild-cd_template.zip $neobuildTemp\neobuild-cd_template.zip
-  expandZip $neobuildTemp\neobuild-cd_template.zip $neobuildData
-  Start-Sleep 5
+  Download $toolsHost/neobuild-cd_template.zip $Path\neobuild-cd_template.zip
+  Expand-Archive -Path $Path\neobuild-cd_template.zip -DestinationPath $Path -Force
 }
 
 function raine {
   Write-Host "setup raine emulator"
-  Download $toolsHost/neobuild-raine.zip $neobuildTemp\neobuild-raine.zip
-  expandZip $neobuildTemp\neobuild-raine.zip $neobuildData
-  Start-Sleep 5
+  Download $toolsHost/neobuild-raine.zip $Path\neobuild-raine.zip
+  Expand-Archive -Path $Path\neobuild-raine.zip -DestinationPath $Path -Force
 }
 
 function raineConfig {
   Write-Host "configure raine"
-  $content = [System.IO.File]::ReadAllText("$neobuildData\raine\config\raine32_sdl.cfg").Replace("/*neocd_bios*/","neocd_bios = $env:appdata\neocore\raine\roms\NEOCD.BIN")
-  [System.IO.File]::WriteAllText("$neobuildData\raine\config\raine32_sdl.cfg", $content)
+  $content = [System.IO.File]::ReadAllText("$Path\raine\config\raine32_sdl.cfg").Replace("/*neocd_bios*/","neocd_bios = $Path\raine\roms\NEOCD.BIN")
+  [System.IO.File]::WriteAllText("$Path\raine\config\raine32_sdl.cfg", $content)
 }
 
 function bin {
   Write-Host "setup bin"
-  Download $toolsHost/neocore-bin.zip $neobuildTemp\neocore-bin.zip
-  expandZip $neobuildTemp\neocore-bin.zip $neobuildData
-  Start-Sleep 5
+  Download $toolsHost/neocore-bin.zip $Path\neocore-bin.zip
+  Expand-Archive -Path $Path\neocore-bin.zip -DestinationPath $Path -Force
 }
 
 function sdk {
   Write-Host "setup sdk"
-  Download $toolsHost/neodev-sdk.zip $neobuildTemp\neodev-sdk.zip
-  expandZip $neobuildTemp\neodev-sdk.zip $neobuildData
-  Start-Sleep 5
+  Download $toolsHost/neodev-sdk.zip $Path\neodev-sdk.zip
+  Expand-Archive -Path $Path\neodev-sdk.zip -DestinationPath $Path -Force
 }
 
 function Download([String] $url, [String] $targetFile){
@@ -47,20 +43,9 @@ function Download([String] $url, [String] $targetFile){
   Write-Output "Time taken: $((Get-Date).Subtract($start_time).Seconds) second(s)"
 }
 
-function expandZip([String] $file, [String] $destination) {
-  Write-Host "expand $file $destination"
-  $shell = new-object -com shell.application
-  $zip = $shell.NameSpace($file)
-  foreach($item in $zip.items()) {
-    $shell.Namespace($destination).copyhere($item)
-  }
-}
-
 function Main {
-  if ((Test-Path -Path $neobuildData) -eq $true) { Remove-Item -Force -Recurse -Path $neobuildData }
-  if ((Test-Path -Path $neobuildTemp) -eq $true) { Remove-Item -Force -Recurse -Path $neobuildTemp }
-  New-Item -ItemType Directory -Force -Path $neobuildData
-  New-Item -ItemType Directory -Force -Path $neobuildTemp
+  if ((Test-Path -Path $Path) -eq $true) { Remove-Item -Force -Recurse -Path $Path }
+  New-Item -ItemType Directory -Force -Path $Path
   sdk
   cdTemplate
   raine
